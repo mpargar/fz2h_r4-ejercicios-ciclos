b18a08fe5cdcc262619c604ec0736ecc7f75728f
Miguel Parra García <32663960+mpargar@users.noreply.github.com>
2024-03-05 19:23:37 -0700

    Ejercicio ciclos, promedio todo wey

--- 09-promedio.cpp
+++ 09-promedio.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main()
 {
-  int cantidadDeCalificaciones = 0, calificacio = 0, sumaCalificaciones = 0;
+  int cantidadDeCalificaciones = 0;
+  float calificacion = 0, sumaCalificaciones = 0;
   cout << "Ingrese la cantidad de calificaciones que desea promediar: ";
   cin >> cantidadDeCalificaciones;
-  while (cantidadDeCalificaciones > 0)
+  for (int i = cantidadDeCalificaciones; i > 0; i--)
   {
     cout << "Ingrese la calificación: ";
-    cin >> calificacio;
-    sumaCalificaciones += calificacio;
-    cantidadDeCalificaciones--;
+    cin >> calificacion;
+    sumaCalificaciones += calificacion;
   }
   cout << "El promedio de las calificaciones es: " << sumaCalificaciones / cantidadDeCalificaciones << endl;
   return 0;
